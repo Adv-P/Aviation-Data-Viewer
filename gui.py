bdd4927b2e5d8e9a0a17f8187f586d0a7d303dff
advaith <vpalaadvaith@gmail.com>
2026-04-30 19:52:35 -0400

    feat: implemented GUI layout, styling, and basic event handling

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, 
                             QLineEdit, QPushButton, QVBoxLayout)
 from PyQt5.QtCore import Qt
+from data import get_metar_data
 
 #Initializing the UI
 class METARApp(QWidget):
@@ -23,10 +24,18 @@ class METARApp(QWidget):
         self.ceiling_label = QLabel("700 feet AGL") #Ceiling is currently a placeholder
         self.clouds_label = QLabel("few clouds aaat 1400 feet AGL, broken clouds at 7000 feet AGL, overcast cloud deck at 8000 feet AGL") #Cloud cover is currently a placeholder
 
+        #Calling the initUI function
+        self.initUI()
+
+    #Adding more properties to the UI
+    def initUI(self):
+        #Window Title
+        self.setWindowTitle("METAR Viewer")
+
         #Layout
         vbox = QVBoxLayout() #Vertical box layout (vbox)
         vbox.addWidget(self.airportid_label)
-        vbox.addWidget(self.airportid_label)
+        vbox.addWidget(self.airportid_input)
         vbox.addWidget(self.get_metar_button)
         vbox.addWidget(self.temperature_label)
         vbox.addWidget(self.dewpoint_label)
@@ -37,3 +46,60 @@ class METARApp(QWidget):
         vbox.addWidget(self.ceiling_label)
         vbox.addWidget(self.clouds_label)
         self.setLayout(vbox)
+
+        #Centering Eveything:
+        self.airportid_label.setAlignment(Qt.AlignCenter)
+        self.airportid_input.setAlignment(Qt.AlignCenter)
+        self.temperature_label.setAlignment(Qt.AlignCenter)
+        self.dewpoint_label.setAlignment(Qt.AlignCenter)
+        self.altimeter_label.setAlignment(Qt.AlignCenter)
+        self.sea_level_pressure_label.setAlignment(Qt.AlignCenter)
+        self.winds_label.setAlignment(Qt.AlignCenter)
+        self.visibility_label.setAlignment(Qt.AlignCenter)
+        self.ceiling_label.setAlignment(Qt.AlignCenter)
+        self.clouds_label.setAlignment(Qt.AlignCenter)
+        
+        #Unique Id's for the widgets
+        self.airportid_input.setObjectName("airportid_input")
+        self.airportid_label.setObjectName("airportid_label")
+        self.get_metar_button.setObjectName("get_metar_button")
+        self.temperature_label.setObjectName("temperature_label")
+        self.dewpoint_label.setObjectName("dewpoint_label")
+        self.altimeter_label.setObjectName("altimeter_label")
+        self.sea_level_pressure_label.setObjectName("sea_level_pressure_label")
+        self.winds_label.setObjectName("winds_label")
+        self.visibility_label.setObjectName("visibility_label")
+        self.ceiling_label.setObjectName("ceiling_label")
+        self.clouds_label.setObjectName("clouds_label")
+    
+        #Apply CSS qualities for widgets
+        self.setStyleSheet("""
+            QLabel, QPushButton{
+                font-family: calibri;
+                font-size: 25px;
+                font-weight: bold;
+            }
+            QLabel#airportid_label{
+                font-size: 38px;
+                font-style: italic;
+            }
+            QLabel#get_metar_button{
+                font-size: 38px;
+            }
+
+        """)
+
+        #Adding functionality to the button
+        self.get_metar_button.clicked.connect(self.get_metar)
+
+    #Getting the METAR data
+    def get_metar(self):
+        print("Metar") #Placeholder; checking if the button is working
+
+    #Handling errors
+    def errors(self,error):
+        pass
+
+    #Displaying the METAR data
+    def display_metar(self,data):
+        pass
--- main.py
+++ main.py
@@ -9,9 +9,7 @@ from data import get_metar_data
 def get_values():
     metar_text = get_metar_data()
     print(metar_text)
-    
-    temperature = metar_text.split("T")[1].split(" ")[0]
-    print(temperature)
+
 
 #Running the program
 if __name__ == "__main__":
