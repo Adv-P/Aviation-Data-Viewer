e4fca00f5011cd90a7b3271bcf80ec8c1926c374
advaith <vpalaadvaith@gmail.com>
2026-05-04 19:08:27 -0400

    feat: implement tabbed interface

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, 
-                            QLineEdit, QPushButton, QVBoxLayout)
+                            QLineEdit, QPushButton, QVBoxLayout,
+                            QTabWidget,QMessageBox)
 from PyQt5.QtCore import Qt
 from data import get_metar_data
 
@@ -24,8 +25,8 @@ class METARApp(QWidget):
         self.ceiling_label = QLabel("",self)
         self.clouds_label = QLabel("",self)
 
-        #Label to display error:
-        self.display_error = QLabel("", self)
+        #Label to display error
+        self.display_error = QTabWidget(self)
 
         #Calling the initUI function
         self.initUI()
@@ -34,23 +35,39 @@ class METARApp(QWidget):
     def initUI(self):
         #Window Title
         self.setWindowTitle("METAR Viewer")
+        
+        #Adding tabs
+        main_layout = QVBoxLayout()
+        self.tabs = QTabWidget()
 
-        #Layout
-        vbox = QVBoxLayout() #Vertical box layout (vbox)
-        vbox.addWidget(self.airportid_label)
-        vbox.addWidget(self.airportid_input)
-        vbox.addWidget(self.get_metar_button)
-        vbox.addWidget(self.temperature_label)
-        vbox.addWidget(self.dewpoint_label)
-        vbox.addWidget(self.altimeter_label)
-        vbox.addWidget(self.sea_level_pressure_label)
-        vbox.addWidget(self.winds_label)
-        vbox.addWidget(self.visibility_label)
-        vbox.addWidget(self.ceiling_label)
-        vbox.addWidget(self.clouds_label)
-        self.setLayout(vbox)
+        #Adding widgets to the "Overview" tab
+        self.overview_tab = QWidget()
+        overview_layout = QVBoxLayout()
+        overview_layout.addWidget(self.airportid_label)
+        overview_layout.addWidget(self.airportid_input)
+        overview_layout.addWidget(self.get_metar_button)
+        self.overview_tab.setLayout(overview_layout)
 
-        #Centering Eveything:
+        #Adding widgets to the "Details" tab
+        self.detail_tab = QWidget()
+        detail_layout = QVBoxLayout()
+        detail_layout.addWidget(self.temperature_label)
+        detail_layout.addWidget(self.dewpoint_label)
+        detail_layout.addWidget(self.altimeter_label)
+        detail_layout.addWidget(self.sea_level_pressure_label)
+        detail_layout.addWidget(self.winds_label)
+        detail_layout.addWidget(self.visibility_label)
+        detail_layout.addWidget(self.ceiling_label)
+        detail_layout.addWidget(self.clouds_label)
+        self.detail_tab.setLayout(detail_layout)
+
+        #Naming the tabs and setting the layout
+        self.tabs.addTab(self.overview_tab, "Overview")
+        self.tabs.addTab(self.detail_tab, "Details")  
+        main_layout.addWidget(self.tabs)
+        self.setLayout(main_layout)  
+
+        #Centering Eveything
         self.airportid_label.setAlignment(Qt.AlignCenter)
         self.airportid_input.setAlignment(Qt.AlignCenter)
         self.temperature_label.setAlignment(Qt.AlignCenter)
@@ -101,7 +118,7 @@ class METARApp(QWidget):
     def get_metar(self):
         airport_id = self.airportid_input.text().upper()
         
-        #Handles errors
+        #Displaying text
         try:
             metar_text = get_metar_data()
             if metar_text:
@@ -110,8 +127,10 @@ class METARApp(QWidget):
                     clean_line = line.strip()
                     if "Temperature" in clean_line:
                         self.temperature_label.setText(clean_line)
-                    elif "Dew point" in clean_line:
+                    elif "Dewpoint" in clean_line:
                         self.dewpoint_label.setText(clean_line)
+                    elif "Sea level pressure" in clean_line:
+                        self.sea_level_pressure_label.setText(clean_line)
                     elif "Wind" in clean_line:
                         self.winds_label.setText(clean_line)
                     elif "Visibility" in clean_line:
@@ -125,9 +144,9 @@ class METARApp(QWidget):
                     elif "Pressure" in clean_line:
                         self.sea_level_pressure_label.setText(clean_line)
             else:
-                self.display_error("No data found for this ID! Please eneter another ID.")
+                QMessageBox.critical(self, "ERROR", "Please try a different ID")
         except Exception as e:
-            self.display_error(f"Error {str(e)}")
+            QMessageBox(self, "ERROR", "An error occured:" + str(e))
 
     #Handling errors
     def errors(self,error):
